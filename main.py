171eca24aaa0fcb266b8af81d0c5aee5b5007272
msnkr <74895279+msnkr@users.noreply.github.com>
2021-03-04 16:03:02 +0200

    update

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ import tb_constants
 import logging
 from youtube_search import YoutubeSearch
 import youtube_dl
+import os
 
 
 def start(update, bot):
-    bot.send_message(chat_id=update.effective_chat.id, text='I am a bot. Please use {/download} to download a song and artist.')
+    bot.send_message(chat_id=update.effective_chat.id, text='I am a bot.')
 
 
 def get_url(update: Update, context=CallbackContext ):
@@ -21,16 +22,23 @@ def get_url(update: Update, context=CallbackContext ):
     url = 'https://www.youtube.com'
     full_url = url + url.join(new_result)
     get_dl(full_url)
-
-
-def get_dl(full_url):
-    ydl_opts = {}
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([full_url])
     
 
+def get_dl(full_url):
+    ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }]}
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([full_url])
+ 
+
 def aud(update, context):
-    context.bot.send_audio(chat_id=update.effective_chat.id, audio=open(f'{full_url}.mp3', 'rb'))
+    for audio_file in os.listdir(r'C:\Users\Digital\Documents\Mikyle\PyFiles'):
+        context.bot.send_audio(chat_id=update.effective_chat.id, audio=open(f'{audio_file}.mp3', 'rb'))
 
 
 def main():
@@ -42,6 +50,7 @@ def main():
     dispatcher.add_handler(start_handler)
 
     updater.dispatcher.add_handler(CommandHandler('download', get_url))
+
 
     updater.start_polling()
     updater.idle()
