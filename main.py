60510dd24182945f423b2716ff957ea404c81965
msnkr <74895279+msnkr@users.noreply.github.com>
2021-11-12 15:58:56 +0200

    Updates

--- main.py
+++ main.py
@@ -1,72 +1,104 @@
 from __future__ import unicode_literals
 from telegram.ext import Updater, Dispatcher, CommandHandler, MessageHandler, Filters, CallbackContext
-from telegram import Update, Audio
-import tb_constants
+from telegram import Update, Audio, Video
 import logging
 from youtube_search import YoutubeSearch
 import youtube_dl
 import os
 
-try:
-    def start(update, context):
-        context.bot.send_message(chat_id=update.effective_chat.id, text='I am a bot. Type {/search} to search artist and song name. You will recieve a message when the file is ready for download. Example {/search Joji Gimme Love}. ')
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text='I am a bot. Type {/audio {artist song}} to search artist and song name audio. Type {/video} to search artist and song video. You will recieve a message when the file is ready for download. Example {/video Joji Gimme Love}. ')
 
 
-    def get_url(update: Update, context=CallbackContext):
+def get_audio_url(update: Update, context=CallbackContext):
+    for file in os.listdir('/home/pi'):
+            if file.endswith('.mp3'):
+                os.remove(file)
 
-        for file in os.listdir('/home/pi'):
-                if file.endswith('.mp3'):
-                    os.remove(file)
+    text = update.message.text
+    results = YoutubeSearch(text, max_results=1).to_json()
 
-        text = update.message.text
-        results = YoutubeSearch(text, max_results=1).to_json()
+    yt_result = results.split('"')
+    new_result = yt_result[-2:-1]
 
-        yt_result = results.split('"')
-        new_result = yt_result[-2:-1]
+    url = 'https://www.youtube.com'
+    full_url = url + url.join(new_result)
+    print(full_url)
+    # get_audio_dl(full_url)
+    # context.bot.send_message(chat_id=update.effective_chat.id, text='The file is finished downloading. Type {/download} to download the file. ')
+    
 
-        url = 'https://www.youtube.com'
-        full_url = url + url.join(new_result)
-        get_dl(full_url)
-        context.bot.send_message(chat_id=update.effective_chat.id, text='The file is finished downloading. Type {/download} to download the file. ')
+def get_audio_dl(full_url):
+    ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }]}
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([full_url])
         
 
-    def get_dl(full_url):
-        ydl_opts = {
-        'format': 'bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-        }]}
-
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([full_url])
-            
-
-    def get_audio(update, context):       
-            for file in os.listdir('/home/pi'):
-                if file.endswith('.mp3'):
-                    context.bot.send_audio(chat_id=update.effective_chat.id, audio=open(f'{file}', 'rb'))
-                    os.remove(file)
+def get_audio(update, context):       
+        for file in os.listdir(r'C:\Users\Digital\Documents\GitHub\tgram_ytbot'):
+            if file.endswith('.mp3'):
+                context.bot.send_audio(chat_id=update.effective_chat.id, audio=open(f'{file}', 'rb'))
+                os.remove(file)
 
 
-    def main():
-        updater = Updater(token=tb_constants.api_key)
-        dispatcher = updater.dispatcher
-        logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+def get_video_url(update: Update, context=CallbackContext):
+    for file in os.listdir(r'C:\Users\Digital\Documents\GitHub\tgram_ytbot'):
+            if file.endswith('.mp4'):
+                os.remove(file)
 
-        start_handler = CommandHandler('start', start)
-        dispatcher.add_handler(start_handler)
+    text = update.message.text
+    results = YoutubeSearch(text, max_results=1).to_json()
 
-        updater.dispatcher.add_handler(CommandHandler('search', get_url))
-        updater.dispatcher.add_handler(CommandHandler('download', get_audio))
+    yt_result = results.split('"')
+    new_result = yt_result[-2:-1]
 
-        updater.start_polling()
-        updater.idle()
-
-
-    main()
-
+    url = 'https://www.youtube.com'
+    full_url = url + url.join(new_result)
+    get_video_dl(full_url)
+    context.bot.send_message(chat_id=update.effective_chat.id, text='The file is finished downloading. Type {/download} to download the file. ')
     
-except FileNotFoundError:
-    print('File not found error.')
+
+def get_video_dl(full_url):
+    ydl_opts = {
+    'format': 'bestvideo/best',
+    'postprocessors': [{
+        'preferredcodec': 'mp4',
+    }]}
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([full_url])
+        
+
+def get_video(update, context):       
+        for file in os.listdir('/home/pi'):
+            if file.endswith('.mp3'):
+                context.bot.send_audio(chat_id=update.effective_chat.id, video=open(f'{file}', 'rb'))
+                os.remove(file)
+
+
+def main():
+    updater = Updater(token='2142016721:AAE22YldNZti1624JLxBuAS6ko5oUqAX51I')
+    dispatcher = updater.dispatcher
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
+
+    updater.dispatcher.add_handler(CommandHandler('audio', get_audio_url))
+    updater.dispatcher.add_handler(CommandHandler('video', get_video_url))
+    updater.dispatcher.add_handler(CommandHandler('downloadAudio', get_audio))
+    updater.dispatcher.add_handler(CommandHandler('downloadVideo', get_video))
+
+    updater.start_polling()
+    updater.idle()
+
+
+main()
