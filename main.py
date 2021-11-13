b781e745ce2a850c441d3a224fbe9f4f7f0808b2
msnkr <74895279+msnkr@users.noreply.github.com>
2021-11-13 11:02:58 +0200

    Updates and Fixes

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ import os
 
 try:
     def start(update, context):
-        context.bot.send_message(chat_id=update.effective_chat.id, text='I am a bot. Type {/audio {artist song}} to search artist and song name audio. Type {/video {artist video}} to search artist and song video. You will recieve a message when the file is ready for download. Example {/video Joji Gimme Love}, {/audio Joji Gimme Love} ')
+        context.bot.send_message(chat_id=update.effective_chat.id, text='I am a bot. Type {/audio {artist song}} to search artist and song name audio. Type {/video {artist video}} to search artist and song video. You will recieve a message when the file is ready for download. Example {/video Joji Gimme Love}, {/audio Joji Gimme Love}. Press {/help} for a list of current problems. ')
+
+    def help(update, context):
+            context.bot.send_message(chat_id=update.effective_chat.id, text='Takes long to download: The Youtube-dl api seems to be capped at 50Kpbs. Unfortunately there isn\'t much I can do about this. The original maintainer of the repo would have to fix this. I might try bypass youtube-dl but that is for a future release. ')
 
 
     def get_audio_url(update: Update, context=CallbackContext):
@@ -86,10 +89,11 @@ try:
         start_handler = CommandHandler('start', start)
         dispatcher.add_handler(start_handler)
 
+        updater.dispatcher.add_handler(CommandHandler('help', help))
         updater.dispatcher.add_handler(CommandHandler('audio', get_audio_url))
         updater.dispatcher.add_handler(CommandHandler('video', get_video_url))
-        updater.dispatcher.add_handler(CommandHandler('downloadaudio', get_audio))
-        updater.dispatcher.add_handler(CommandHandler('downloadvideo', get_video))
+        updater.dispatcher.add_handler(CommandHandler('da', get_audio))
+        updater.dispatcher.add_handler(CommandHandler('dv', get_video))
 
         updater.start_polling()
         updater.idle()
